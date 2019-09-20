b25e927db4f30abf7d1a357b97b599a522541a51
aaaatrox <2104934938@qq.com>
2019-09-20 16:10:40 +0800

    监听esc关闭

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -44,30 +44,30 @@ MainWindow::MainWindow(QWidget *parent) :
 bool MainWindow::eventFilter(QObject *watched, QEvent *event)
 {
     qDebug() << __FILE__;
+    qDebug() << __FUNCTION__;
     qDebug() << watched->objectName();
     qDebug() << event->type();
-    qDebug() << __FUNCTION__;
-//    if (event->type() == QEvent::KeyPress) {
-//        qDebug() << "key pressed";
-//        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
-//        MainWindow::keyPressEvent(keyEvent);
-//        if (keyEvent->key() == Qt::Key_Escape) {
-//            this->close();
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    } else {
-//        return false;
-//    }
+    if (event->type() == QEvent::ShortcutOverride) {
+        qDebug() << "key pressed";
+        QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
+        qDebug() << keyEvent->key();
+        if (keyEvent->key() == Qt::Key_Escape) {
+            this->close();
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
 }
 
 void MainWindow::keyPressEvent(QKeyEvent *event)
 {
-    qDebug() << __FILE__;
-    qDebug() << __FUNCTION__;
-    qDebug() << "key pressed";
-    QMainWindow::keyPressEvent(event);
+//    qDebug() << __FILE__;
+//    qDebug() << __FUNCTION__;
+//    qDebug() << "key pressed";
+//    QMainWindow::keyPressEvent(event);
 }
 
 MainWindow::~MainWindow()
