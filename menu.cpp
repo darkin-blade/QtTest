8d11a322f3b40707bc97cf6747001d7ebf17c0c2
aaaatrox <2104934038@qq.com>
2019-09-21 16:08:21 +0800

    菜单界面

--- menu.cpp
+++ menu.cpp
@@ -8,7 +8,13 @@ Menu::Menu(QWidget *parent) : QMainWindow(parent)
 
 void Menu::init()
 {
-    this->setFixedSize(500, 300);// 菜单窗口的大小
+    
+    int widthMenu = 200;
+    int heightMenu = 170;
+    int widthBtn = 120;
+    int heightBtn = 30;
+
+    this->setFixedSize(widthMenu, heightMenu);// 菜单窗口的大小
     this->setWindowTitle("menu");
 
     // 托盘图标
@@ -16,28 +22,38 @@ void Menu::init()
     tray->setIcon(QIcon(QPixmap("/home/lynx/Pictures/Aatrox256.png")));
     tray->show();
     connect(tray, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this, SLOT(TrayIconAction(QSystemTrayIcon::ActivationReason)));
-    
-    int width = this->width();
-    int height = this->height();
 
     btnQuit = new QPushButton(this);
     btnQuit->setText("quit");
-    btnQuit->setFixedSize(100, 40);
-    btnQuit->move(width - 100, height - 60);
+    btnQuit->setFixedSize(widthBtn, heightBtn);
+    btnQuit->move(40, (heightBtn + 10) * 3);
 
     btnShow = new QPushButton(this);
     btnShow->setText("show");
-    btnShow->setFixedSize(100, 40);
-    btnShow->move(width - 200, height - 60);
+    btnShow->setFixedSize(widthBtn, heightBtn);
+    btnShow->move(40, (heightBtn + 10) * 2);
 
     btnHide = new QPushButton(this);
     btnHide->setText("hide");
-    btnHide->setFixedSize(100, 40);
-    btnHide->move(width - 300, height - 60);
+    btnHide->setFixedSize(widthBtn, heightBtn);
+    btnHide->move(40, (heightBtn + 10) * 1);
     
     connect(btnQuit, SIGNAL(clicked(bool)), this, SLOT(pressQuit()));
     connect(btnShow, SIGNAL(clicked(bool)), this, SLOT(pressShow()));
     connect(btnHide, SIGNAL(clicked(bool)), this, SLOT(pressHide()));
+
+    this->setStyleSheet(
+        "Menu {"
+        "   background: rgb(44, 45, 45);"
+        "   border-radius: 100px;"
+        "}"
+        "QPushButton {"
+        "   font-size: 18px;"
+        "   background: rgb(240, 240, 240);"
+        "   border: 0px solid red;"
+        "   border-radius: 1px;"
+        "}"
+    );
 
     // 背景
     background = new MainWindow();
@@ -50,7 +66,7 @@ void Menu::pressQuit()
 
 void Menu::pressHide()
 {
-    background->hide();
+    background->close();
 }
 
 void Menu::pressShow()
