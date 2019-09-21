b27f0479d36221639a63d87f6223c288f3b3b690
aaaatrox <2104934038@qq.com>
2019-09-21 10:11:14 +0800

    照搬网上代码

--- menu.cpp
+++ menu.cpp
@@ -2,5 +2,42 @@
 
 Menu::Menu(QWidget *parent) : QMainWindow(parent)
 {
+    init();
+}
 
+void Menu::init()
+{
+    this->setFixedSize(200, 200);
+    this->setWindowTitle("menu");
+
+    tray = new QSystemTrayIcon(this);
+    tray->setIcon(QIcon(QPixmap("/home/lynx/Pictures/Aatrox256.png")));
+    tray->show();
+    connect(tray, SIGNAL(activated(QSystemTrayIcon::ActivationReason)), this, SLOT(TrayIconAction(QSystemTrayIcon::ActivationReason)));
+
+    menu = new QMenu(this);
+    reset = new QAction(this);
+    reset->setText("show");
+    connect(reset, SIGNAL(triggered()), this, SLOT(showNormal()));
+
+    quit = new QAction(this);
+    quit->setText("quit");
+    connect(quit, SIGNAL(triggered()), qApp, SLOT(quit()));
+
+    tray->setContextMenu(menu);
+    menu->addAction(reset);
+    menu->addAction(quit);
+}
+
+void Menu::closeEvent(QCloseEvent *event)
+{
+    hide();
+    event->ignore();
+}
+
+void Menu::TrayIconAction(QSystemTrayIcon::ActivationReason reason)
+{
+    if (reason == QSystemTrayIcon::Trigger) {
+        this->showNormal();
+    }
 }
--- menu.h
+++ menu.h
@@ -2,16 +2,31 @@
 #define MENU_H
 
 #include <QMainWindow>
+#include <QSystemTrayIcon>
+#include <QMenu>
+#include <QAction>
+#include <QApplication>
+#include <QtGui>
 
 class Menu : public QMainWindow
 {
     Q_OBJECT
 public:
     explicit Menu(QWidget *parent = nullptr);
+    void init();// 初始化函数
+ 
+    QSystemTrayIcon *tray;// 托盘图标
+    QMenu *menu;// 托盘菜单
+    QAction *reset;// 菜单实现功能:恢复窗口
+    QAction *quit;// 菜单实现功能:退出程序
+
+protected:
+    void closeEvent(QCloseEvent *event);
 
 signals:
 
 public slots:
+    void TrayIconAction(QSystemTrayIcon::ActivationReason reason);// 对托盘图标操作的槽:本代码实现单机图标恢复窗口功能
 };
 
 #endif // MENU_H
